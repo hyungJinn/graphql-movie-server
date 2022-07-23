8829fed0bc7ce0c52fec6aea617fea2da80bd5f0
HyungJin <hyungjin0710@gmail.com>
2022-07-23 16:38:12 +0900

    #4.3 Mutation Type

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 import { ApolloServer, gql } from "apollo-server";
 
+// GET /api/v1/tweets
+// POST /api/v1/tweets
+// GET /api/v1/tweet/:id
+
 const typeDefs = gql`
   type User {
     id: ID
     username: String
   }
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet!
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
