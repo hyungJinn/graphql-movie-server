be664c9fdf3c7a1cbe3580e3c1be129357e6c4e0
HyungJin <hyungjin0710@gmail.com>
2022-07-24 12:41:09 +0900

    #4.6 Query Resolvers

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  {
+    id: "1",
+    text: "first one!",
+  },
+  {
+    id: "2",
+    text: "second one!",
+  },
+];
+
 // GET /api/v1/tweets
 // POST /api/v1/tweets
 // GET /api/v1/tweet/:id
 
+// gql`SDL` - SDL(Schema Definition Language)
 const typeDefs = gql`
   type User {
     id: ID!
@@ -14,7 +26,7 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
@@ -29,7 +41,18 @@ const typeDefs = gql`
 // tweet(id: ID): Tweet
 // It could be an ID or It could be a null
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
